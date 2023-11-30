dd3aaf7da6f4b2ed060c7c6954842e64ef919ba6
TobyJWalker <tobyjwalker03@outlook.com>
2023-11-30 23:48:22 +0000

    implemented update handler

--- handler/order.go
+++ handler/order.go
@@ -131,7 +131,92 @@ func (o *Order) GetByID(w http.ResponseWriter, r *http.Request) {
 
 // update by id method
 func (o *Order) UpdateByID(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("update order by id")
+	
+	// capture body structure
+	var body struct {
+		Status string `json:"status"`
+	}
+
+	// json decode to check for bad request
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// get id from url
+	idParam := chi.URLParam(r, "id")
+
+	// convert to uint64
+	const base = 10
+	const bitSize = 64
+	orderID, err := strconv.ParseUint(idParam, base, bitSize)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// get order
+	order, err := o.Repo.FindByID(r.Context(), orderID)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// init constants
+	const completedStatus = "completed"
+	const shippedStatus = "shipped"
+	
+	// get current time
+	now := time.Now().UTC()
+
+	// check status
+	switch body.Status {
+	case shippedStatus:
+
+		// check if order is already shipped
+		if order.ShippedAt != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		
+		order.ShippedAt = &now // set shipped at time
+	case completedStatus:
+
+		// check if order is already completed or not shipped
+		if order.CompletedAt != nil || order.ShippedAt == nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		order.CompletedAt = &now // set completed at time
+	
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// update order
+	err = o.Repo.Update(r.Context(), order)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// marshall order to json
+	res, err := json.Marshal(order)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// write response
+	w.Write(res)
+	w.WriteHeader(http.StatusOK)
+
 }
 
 // delete method
