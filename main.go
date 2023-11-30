5e135cae03d3b80615799a449b62a5c3abeb9eaf
TobyJWalker <tobyjwalker03@outlook.com>
2023-11-30 20:37:43 +0000

    moved contents to other files, use application package

--- main.go
+++ main.go
@@ -1,40 +1,21 @@
 package main
 
 import (
-	"net/http" // http package
-
-	"github.com/go-chi/chi/v5"            // chi plugin
-	"github.com/go-chi/chi/v5/middleware" // chi middleware
+	"chi-orders-api/application" // import application package
+	"context"
+	"fmt"
 )
 
 func main() {
 
-	// create chi router
-	router := chi.NewRouter()
+	// create new app
+	app := application.New()
 
-	// add middleware
-	router.Use(middleware.Logger) // log requests
+	// start app
+	err := app.Start(context.TODO())
 
-	// create route
-	router.Get("/", basicHandler)
-
-	// create server
-	server := &http.Server{
-		Addr: ":3000", // port
-		Handler: router, // interface when server recieves request
-	}
-
-	// start server
-	err := server.ListenAndServe()
-
-	// check for errors
+	// handle errors
 	if err != nil {
-		panic(err)
+		fmt.Printf("error starting app: %s\n", err)
 	}
-
-}
-
-// create basic http handler
-func basicHandler(w http.ResponseWriter, r *http.Request) { // w = response writer, r = request
-	w.Write([]byte("Hello World")) // write to response writer
 }
