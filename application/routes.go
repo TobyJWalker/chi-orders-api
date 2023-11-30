3beec8cf7b27525faf631fe02f7947937065a305
TobyJWalker <tobyjwalker03@outlook.com>
2023-11-30 20:45:46 +0000

    created order routes

--- application/routes.go
+++ application/routes.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+
+	"chi-orders-api/handler"
 )
 
 // loadRoutes function to load routes
@@ -16,10 +18,28 @@ func loadRoutes() *chi.Mux {
 	// add middleware
 	router.Use(middleware.Logger)
 
-	// create route
+	// create index route
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
 
+	// orders route
+	router.Route("/orders", loadOrderRoutes)
+
 	return router
+}
+
+// loadOrderRoutes function to load order routes
+func loadOrderRoutes(router chi.Router) {
+
+	// create order handler
+	orderHandler := &handler.Order{}
+
+	// create routes for http methods
+	router.Post("/", orderHandler.Create)
+	router.Get("/", orderHandler.List)
+	router.Get("/{id}", orderHandler.GetByID)
+	router.Put("/{id}", orderHandler.UpdateByID)
+	router.Delete("/{id}", orderHandler.Delete)
+
 }
