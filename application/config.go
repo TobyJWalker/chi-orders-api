72cdff889933a243bc733d36091ce0970e701ca9
TobyJWalker <tobyjwalker03@outlook.com>
2023-12-01 19:16:20 +0000

    added app_env config

--- application/config.go
+++ application/config.go
@@ -8,18 +8,24 @@ import (
 // configuration structure
 type Config struct {
 	ServerPort uint16
+	Environment string
 }
 
 // function to load configs
 func LoadConfig() Config {
 	cfg := Config{
 		ServerPort: 3000,
+		Environment: "development",
 	}
 
 	if serverPort, exists := os.LookupEnv("SERVER_PORT"); exists {
 		if port, err := strconv.ParseUint(serverPort, 10, 16); err == nil {
 			cfg.ServerPort = uint16(port)
 		}
+	}
+
+	if app_env, exists := os.LookupEnv("APP_ENV"); exists {
+		cfg.Environment = app_env
 	}
 
 	return cfg
